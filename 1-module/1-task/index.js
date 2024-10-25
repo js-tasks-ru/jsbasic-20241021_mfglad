5463651b1e073ea3a6d6a9b9ee112c7cda8a02e1
maxfedorov <mfglad@gmail.com>
2024-10-26 01:38:03 +0200

    1 module tasks completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  let result = n
+
+  for (let i = n - 1; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 }
