906cb2aece19a850eef51e906ebfc9e26b287719
“maxfedorov” <mfglad@gmail.com>
2024-11-11 23:39:35 +0100

    5 module tasks completed

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,24 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRightButton = document.querySelector(".carousel__arrow_right");
+  let arrowLeftButton = document.querySelector(".carousel__arrow_left");
+  let carousel = document.querySelector(".carousel");
+  let carouselInner = document.querySelector(".carousel__inner");
+  let carouselWidth = carouselInner.offsetWidth;
+  let currentSlide = 0;
+  const SLIDES_COUNT = 4;
+
+  function showArrows() {
+    arrowRightButton.style.display = currentSlide === SLIDES_COUNT - 1 ? "none" : "";
+    arrowLeftButton.style.display = currentSlide < 1 ? "none" : "";
+  }
+
+  showArrows();
+
+  carousel.addEventListener("click", (event) => {
+    let arrowButton = event.target.closest('.carousel__arrow');
+    if (!arrowButton) return;
+    currentSlide = arrowButton == arrowRightButton ? currentSlide + 1 : currentSlide - 1;
+    carouselInner.style.transform = `translateX(-${carouselWidth * currentSlide}px)`;
+    showArrows()
+  });
 }
