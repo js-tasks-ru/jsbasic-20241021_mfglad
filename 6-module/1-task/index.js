3c9037e1aa015226b6789234584e394a7fceee90
“maxfedorov” <mfglad@gmail.com>
2024-11-23 02:44:04 +0100

    6 module tasks complete

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,45 @@
  *
  */
 export default class UserTable {
+
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.#render();
+  }
+
+  #template() {
+    return `
+    <table>
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      ${this.#rows.map(row => `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button>X</button></td></tr>`).join('\n')}
+      </tbody>
+    </table>
+    `
+  }
+
+  #render() {
+    const temp = document.createElement('div');
+    temp.innerHTML = this.#template();
+    this.elem = temp.firstElementChild;
+
+    this.elem.querySelectorAll('tr td button').forEach(element => {
+      element.addEventListener('click', (event) => this.#removeRow(event), { once: true });
+    });
+  }
+
+  #removeRow(event) {
+    event.target.closest('tr').remove();
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,80 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+  elem = null;
+  slides = null;
+
+  get elem() {
+    return this.elem;
+  }
+
   constructor(slides) {
     this.slides = slides;
+    this.#renderCarousel();
+    this.#initCarousel();
+  }
+
+  #template() {
+    return `
+  <div class="carousel">
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+
+    <div class="carousel__inner">
+      ${this.slides.map(slide => `
+        <div class="carousel__slide" data-id="${slide.id}">
+          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+      `).join('\n')}
+    </div>
+  </div>`
+  }
+
+  #renderCarousel() {
+    this.elem = createElement(this.#template());
+
+    this.elem.querySelectorAll('.carousel__button').forEach(button => {
+      button.addEventListener('click', (event) => {
+        this.elem.dispatchEvent(
+          new CustomEvent("product-add", {
+            detail: event.target.closest('.carousel__slide').dataset.id,
+            bubbles: true
+          })
+        )
+      });
+    })
+  }
+
+  #showCarouselArrows() {
+    this.arrowRightButton.style.display = this.currentSlide === this.slides.length - 1 ? "none" : "";
+    this.arrowLeftButton.style.display = this.currentSlide < 1 ? "none" : "";
+  }
+
+  #initCarousel() {
+    this.arrowRightButton = this.elem.querySelector(".carousel__arrow_right");
+    this.arrowLeftButton = this.elem.querySelector(".carousel__arrow_left");
+    this.carouselInner = this.elem.querySelector(".carousel__inner");
+    this.currentSlide = 0;
+
+    this.#showCarouselArrows();
+
+    this.elem.addEventListener("click", (event) => {
+      let arrowButton = event.target.closest('.carousel__arrow');
+      if (!arrowButton) return;
+      this.currentSlide = arrowButton == this.arrowRightButton ? this.currentSlide + 1 : this.currentSlide - 1;
+      this.carouselInner.style.transform = `translateX(-${this.carouselInner.offsetWidth * this.currentSlide}px)`;
+      this.#showCarouselArrows();
+    });
   }
 }
